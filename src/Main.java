4ae6253164418f9ea0dd743e62b9498bed242710
Rafael <rafael.akhmetsagirov@mail.ru>
2022-02-28 22:37:45 +0400

    Edit request main

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,14 @@ class Main {
         int speedOfPlayer3 = 2;
         int outPlayers = 0;
 
-        if (isGreenLight = true) {
-            if (speedOfPlayer1 >= 1) {
+        if (isGreenLight == false) {
+            if (speedOfPlayer1 > 0) {
                 outPlayers = outPlayers + 1;
             }
-            if (speedOfPlayer2 >= 1) {
+            if (speedOfPlayer2 > 0) {
                 outPlayers = outPlayers + 1;
             }
-            if (speedOfPlayer3 >= 1) {
+            if (speedOfPlayer3 > 0) {
                 outPlayers = outPlayers + 1;
             }
 
