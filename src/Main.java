20e5101a004b551e792226cb2ea113aedb832e79
Rafael <rafael.akhmetsagirov@mail.ru>
2022-03-02 17:10:31 +0400

    Stat Method Stat Pole HW 16

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,40 @@
-class Main {
-    public static void main(String[] args) {
-        boolean isGreenLight = false;
+import my.favorite.code.Utils;
 
-        int speedOfPlayer1 = 5;
+public class Main {
+    public static boolean isReedLight = false;
+
+    public static void main(String[] args){
+
+
+        int speedOfPlayer1 = 1;
         int speedOfPlayer2 = 0;
         int speedOfPlayer3 = 2;
         int outPlayers = 0;
 
-        if (isGreenLight == false) {
-            if (speedOfPlayer1 > 0) {
+
+        if(isReedLight == false)
+
+        {
+            if (speedOfPlayer1 >= 1) {
                 outPlayers = outPlayers + 1;
+                Utils.printDelim1("Статус игрока");
             }
-            if (speedOfPlayer2 > 0) {
+            if (speedOfPlayer2 >= 1) {
                 outPlayers = outPlayers + 1;
+                Utils.printDelim2("Статус игрока");
             }
-            if (speedOfPlayer3 > 0) {
+            if (speedOfPlayer3 >= 1) {
                 outPlayers = outPlayers + 1;
+                Utils.printDelim3("Статус игрока");
             }
 
+            System.out.println("Выбыло: " + outPlayers);
 
+        } else
+
+        {
+            System.out.println("Все в Игре! ");
         }
-        System.out.println("Выбыло: " + outPlayers);
 
     }
 
@@ -28,9 +42,7 @@ class Main {
 }
 
 
-// Допишите здесь логику так, что будет подсчитано и выведено
-// количество игроков, которые выбывают.
-// Если свет зелёный, то проходят все игроки (0 выбывают).
-// Если свет красный, то выбывает каждый, чья скорость не 0.
+
+
 
 
