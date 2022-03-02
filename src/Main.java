60fc9008a7814b9d5ebf4721f65cf9d8cd3974a0
RafaelAkhm <98662815+RafaelAkhm@users.noreply.github.com>
2022-03-02 16:56:07 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,15 @@ class Main {
         int speedOfPlayer3 = 2;
         int outPlayers = 0;
 
-        if (isGreenLight = true)
+        if (isGreenLight == true)
             if (isGreenLight = false){
-            if (speedOfPlayer1 >= 1) {
+            if (speedOfPlayer1 > 0) {
                 outPlayers = outPlayers + 1;
             }
-            if (speedOfPlayer2 >= 1) {
+            if (speedOfPlayer2 > 0) {
                 outPlayers = outPlayers + 1;
             }
-            if (speedOfPlayer3 >= 1) {
+            if (speedOfPlayer3 > 0) {
                 outPlayers = outPlayers + 1;
             }
 
