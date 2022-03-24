39473947faddf01020b1c018fb27c3935bed13dc
RafaelAkhm <98662815+RafaelAkhm@users.noreply.github.com>
2022-03-24 16:52:32 +0400

    with help

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,30 @@ import my.favorite.code.Utils;
 
 class Color {
 
-    public static boolean speedOfPlayer;
-    static boolean isReedLight = false;
+    public static boolean isGreenLight = true;
 
-    public static int speedsOfPlayers(int speedOfPlayer) {
-        speedOfPlayer = 5;
-
-
-        if (isReedLight) {
-            if (speedOfPlayer > 0) {
-                speedOfPlayer = 1;
-                Utils.printDelim1("Статус игрока");
-            } else {
-                Utils.printDelim2("Статус игрока");
-            }
-        }
-        return speedOfPlayer;
+    public static boolean isLose(int speedOfPlayer) {
+       
+        if (isGreenLight) {
+            return false;
+        if (speed > 0) {
+        return true;
+        } else {
+            return false;
     }
-
 }
 
 public class Main {
-    public static void main(String[] args) {
-        if (Color.isReedLight) {
-            System.out.println("Выбыло: " + Color.speedsOfPlayers(1) );
-        } else {
-            System.out.println("Зеленый свет! Передвигайся и оставайся в игре! ");
-        }
+   public static void main(String[] args) {
+        Color.isGreenLight = true;
+        boolean f = Color.isLose();
+        if (f==true) 
+            System.out.println("Игрок выбыл");
+        else
+            System.out.println("Игрок проходит");
+       Color.isGreenLight = true;
+       System.out.println(Color.isLose());
+    
     }
 }
 
