044c456a29829387d8e3f45f6684dfc0a51e8540
RafaelAkhm <98662815+RafaelAkhm@users.noreply.github.com>
2022-03-25 17:13:22 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,32 @@ class Color {
     public static boolean isGreenLight = true;
 
     public static boolean isLose(int speedOfPlayer) {
-       
+        boolean speed = false;
         if (isGreenLight) {
-            return false;
-        if (speed > 0) {
-        return true;
-        } else {
-            return false;
+            if (speedOfPlayer > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return speed;
     }
 }
 
-public class Main {
-   public static void main(String[] args) {
-        Color.isGreenLight = true;
-        boolean f = Color.isLose(10);
-        if (f==true) 
-            System.out.println("Игрок выбыл");
-        else
-            System.out.println("Игрок проходит");
-       Color.isGreenLight = true;
-       System.out.println(Color.isLose(10));
-    
-    }
-}
+        public class Main {
+            public static void main(String[] args) {
+                Color.isGreenLight = true;
+                boolean f = Color.isLose(0);
+                if (f==true)
+                    System.out.println("Игрок выбыл");
+                else
+                    System.out.println("Игрок проходит");
+                Color.isGreenLight = true;
+                System.out.println(Color.isLose(5));
 
+
+            }
+        }
 
 
 
