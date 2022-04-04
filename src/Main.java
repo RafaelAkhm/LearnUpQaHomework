73d13135977bbebc841d323f487ffc543d72e58a
RafaelAkhm <98662815+RafaelAkhm@users.noreply.github.com>
2022-04-04 23:32:06 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,12 @@ public class Main {
         speedOfPlayer[0] = 0;
         speedOfPlayer[1] = 3;
         speedOfPlayer[2] = 2;
-        int numberOfDropouts = Statistic.numberOfDropouts(speedOfPlayer);
-        System.out.println("Количество выбывших игроков " + numberOfDropouts);
-        int[] speedsOfDropped = Statistic.speedsOfDropped(speedOfPlayer);
-        System.out.println("Скорость выбывающих " + Arrays.toString(speedsOfDropped));
-        int[] speedsOfNotDropped = Statistic.speedsOfNotDropped(speedOfPlayer);
-        System.out.println("Скорость не выбывающих " + Arrays.toString(speedsOfNotDropped));
+       int numberOfDropouts = Statistic.numberOfDropouts(speedOfPlayer);
+       System.out.println("Количество выбывших игроков " + numberOfDropouts);
+       int[] speedsOfDropped = Statistic.speedsOfDropped(speedOfPlayer);
+       System.out.println("Скорость выбывающих " + Statistic.speedsOfDropped(speedOfPlayer));
+       int[] speedsOfNotDropped = Statistic.speedsOfNotDropped(speedOfPlayer);
+       System.out.println("Скорость не выбывающих " + Statistic.speedsOfNotDropped(speedOfPlayer));
     }
 }
 
