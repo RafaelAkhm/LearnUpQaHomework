21aa41dffca351b4b0a0a2476bb626c1b2c83712
RafaelAkhm <98662815+RafaelAkhm@users.noreply.github.com>
2022-04-04 22:21:52 +0400

    Create StatisticTest.java

--- src/StatisticTest.java
+++ src/StatisticTest.java
@@ -6,27 +6,29 @@ public class StaticTest {
     @Test
 
     public void NumbersOutPlayers() {
-        int[] speedOfPlayers = {0, 3, 2 };
-
+        int[] speedOfPlayer = {0, 3, 2 };
         int expected = 3;
         int actual = Statistic.numberOfDropouts(speedOfPlayers);
-
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
 
-    public void MassOutPlayers() {
-        int[] speedOfPlayers = {0, 3, 2 };
-
+    public void MassOutPlayer() {
+        int[] speedOfPlayer = {0, 3, 2 };
         int[] expected = {3, 2};
         int[] actual = Statistic.speedsOfDropped(speedOfPlayers);}
     @Test
 
-    public void MassStayPlayers() {
+    public void MassStayPlayer() {
         int[] speedOfPlayers = {0, 3, 2 };
-
         int[] expected = {0};
         int[] actual = Statistic.speedsOfNotDropped(speedOfPlayers);}
 
+    @Test
+
+    public void MAX_SPEED() {
+        int[] speedOfPlayers = {3, 0, 0, 4, 1};
+        int[] expected = {4};
+        int[] actual = Statistic.MAX_SPEED(speedOfPlayers);}
     }
